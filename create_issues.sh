4c22c58ed4fe9488c93499f6dfddedc578bc04a6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-10-07 16:39:20 +0900

    update

--- create_issues.sh
+++ create_issues.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 echo "[INFO] Creating issues" >&2
 while read -r repo; do
 	if echo "$repo" | grep -f created_repos.txt > /dev/null; then
-		echo "[INFO] Skipped creating an issue because it was already created to this repository  repo=$repo" >&2
+		echo "[INFO] Skipped creating an issue because it was already created to this repository  repo=$repo url=https://github.com/$repo" >&2
 		continue
 	fi
-	echo "[INFO] Creating an issue  repo=$repo" >&2
+	echo "[INFO] Creating an issue  repo=$repo url=https://github.com/$repo" >&2
 	sed "s|<package>|$repo|g" body.md |
 		gh -R "$repo" issue create \
 			-F - \
